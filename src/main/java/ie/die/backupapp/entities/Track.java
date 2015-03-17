b1116e9d4d9ef754fe2254275ebaa3900ab11651
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-03-17 20:15:13 +0000

    entities: add basic entities

---
+++ src/main/java/ie/die/backupapp/entities/Track.java
@@ -0,0 +1,10 @@
+package ie.die.backupapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_tracks")
+public class Track {
+
+}
---
+++ src/main/java/ie/die/backupapp/entities/User.java
@@ -0,0 +1,20 @@
+package ie.die.backupapp.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_users")
+//@NamedQueries()
+public class User {
+
+	@Id
+	@Column(length = 32)
+	private String username;
+	@Column(length = 64)
+	private String password;
+	@Column
+	private String libraryPersistentId;
+}
